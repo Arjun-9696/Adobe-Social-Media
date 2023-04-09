127ad18dade977b73f915311e766cb53ce2fa782
Arjun9696 <mallikarjunharalalli@gmail.com>
2023-04-09 18:48:21 +0530

    Updated the env name for mongo url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,7 @@ module.exports = async () => {
 		useUnifiedTopology: true,
     }
     try {
-        await mongoose.connect(process.env.MONGO_URL, connectionParams);
+        await mongoose.connect(process.env.MONGO_URI, connectionParams);
         console.log("Connected to database successfully")
     } catch (error) {
         console.log("Could not connect to database!")
